af8cc363c73d846c21551f5542e5ca37b140a065
Muhammad <davlatovmuhammad1264@mail.com>
2021-02-01 14:44:44 +0500

    Add zadaniya2/

---
+++ zadaniya2/zadaniya2/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace zadaniya2
+{
+	interface IPlayable
+	{
+		void Play();
+		void Pause();
+		void Stop();
+	}
+
+	interface IRecodable
+	{
+		void Play();
+		void Pause();
+		void Stop();
+	}
+
+	class Player : IPlayable
+	{
+		public void Play()
+		{
+			Console.WriteLine("IPlayable.Play");
+		}
+		public void Pause()
+		{
+			Console.WriteLine("IPlayable.Pause");
+		}
+		public void Stop()
+		{
+			Console.WriteLine("IPlayable.Stop");
+		}
+	}
+
+	class Players : IRecodable
+	{
+		public void Play()
+		{
+			Console.WriteLine("IRecodable.Play");
+		}
+		public void Pause()
+		{
+			Console.WriteLine("IRecodable.Pause");
+		}
+		public void Stop()
+		{
+			Console.WriteLine("IRecodable.Stop");
+		}
+		
+	}
+
+	class MainClass
+	{
+		static void Action(IPlayable movable)
+		{
+			movable.Play();
+			movable.Pause();
+			movable.Stop();
+		}
+		static void action(IRecodable Movable)
+		{
+			Movable.Play();
+			Movable.Pause();
+			Movable.Stop();
+		}
+		public static void Main(string[] args)
+		{
+			Player player = new Player();
+			Action(player);
+			Players players = new Players();
+			action(players);
+			Console.ReadKey();
+			
+		}
+	}
+}
